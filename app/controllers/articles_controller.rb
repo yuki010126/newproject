aec2ff12cec0ca552bc02e7caf01fdd8b61aa766
yuki010126 <kinyounokyuzitu@gmail.com>
2021-10-21 14:28:23 +0000

    CRUD完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -19,6 +20,26 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+     if @article.update(article_params)
+       redirect_to @article
+     else
+       render:edit
+     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
 
   private
